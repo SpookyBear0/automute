9e2787d43a4c5a8a97795512768d0bb68d344fe8
Spookybear0 <collinmcarroll@gmail.com>
2020-07-29 23:03:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,27 @@ import math
 import time
 import os
 from pycaw.pycaw import AudioUtilities, ISimpleAudioVolume
+import sys
 
+try:
+    app_to_mute = sys.argv[1]
+except IndexError:
+    app_to_mute = "Discord.exe"
+    
+# setup volume
 volume = None; vol = None
 sessions = AudioUtilities.GetAllSessions()
 for session in sessions:
-    if session.Process and session.Process.name() == "Discord.exe":
+    if session.Process and session.Process.name() == app_to_mute:
         volume = session._ctl.QueryInterface(ISimpleAudioVolume)
         vol = volume.GetMasterVolume()
 
+# vars
 memory = gd.memory.get_memory()
-
-gettingfar = input("what percent to mute at?: ")
 muted = False
+gettingfar = input("what percent to mute at?: ")
 
+# mainloop
 while True:
     memory.reload()
     percent = memory.get_percent()
